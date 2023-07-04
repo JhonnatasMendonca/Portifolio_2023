8cd5b62e73d4ad80b38bb3d9ff67eb8accc0fd6f
Jhonnatas Mendonca <jhonnatas.m2000@gmail.com>
2023-07-04 04:49:31 -0300

    Style: responsivo (max: 820px) criado.

--- functions.js
+++ functions.js
@@ -1,3 +1,32 @@
+// Funções usando Jquery.
+
+document.addEventListener('DOMContentLoaded', function() {
+    //Ativar carrossel
+
+    $('.autoplay').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+    });
+
+    //Abrir menu mobile
+
+    $('#menu_hamburguer').click(function(){
+
+        if($(".lista_menu_mobile").css("display") == "none"){
+            $('.lista_menu_mobile').css("display", "flex");
+        }else{
+            $('.lista_menu_mobile').css("display", "none");
+        }
+
+    });
+
+});
+
+// Funções usando JS Puro.
+
 function redirecionaprotea(){
     let linktest = document.getElementById('protea');
     linktest.onclick = function () {
